ae90f075d417449fa09a0fd9e710d26abd6dff91
Dubinets Aleksandr <dubinets.av@gmail.com>
2013-04-06 20:28:01 +1100

    1126 - Магнитные бури

---
+++ timus/1126 - Магнитные бури.cpp
@@ -0,0 +1,24 @@
+// 1126. Магнитные бури
+// 0.234 s
+// 0.234
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+
+int main()
+{
+	int		m;
+	long	data[25005] = {-1};
+	int		ilast;
+	
+	std::cin >> m;
+	for(int i=0;std::cin>>data[i];i++)
+		ilast = i;
+
+	for(auto p=data; p + m!=data + ilast + 1; p++)
+		std::cout << *std::max_element(p,p+m) << std::endl;
+
+	return 0;
+}
