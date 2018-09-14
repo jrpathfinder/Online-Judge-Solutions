96692630d300915075e2c3741df35c002c27da21
Dubinets A.V <dubinets.av@gmail.com>
2018-09-14 22:20:16 +0300

    1563, 1881

---
+++ timus/1881 - Длинное условие задачи.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+        Reader reader = oj ? new InputStreamReader(System.in) : new FileReader("input.txt");
+        Writer writer = oj ? new OutputStreamWriter(System.out) : new FileWriter("output.txt");
+        StreamTokenizer in = new StreamTokenizer(new BufferedReader(reader));
+        PrintWriter out = new PrintWriter(writer);
+
+        BufferedReader reader1 = new BufferedReader(reader);
+        BufferedWriter writer1 = new BufferedWriter(writer);
+
+        Integer n = Integer.parseInt(reader1.readLine());
+        Integer counter = 0;
+
+        ArrayList<String> guests = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            guests.add(reader1.readLine());
+            counter++;
+            if(guests.get(i).contains("+one")) counter++;
+        }
+
+        if (((counter + 2) == 13)) {
+            writer1.write("1400");
+        } else {
+            writer1.write(new Integer ((counter + 2) * 100).toString());
+        }
+        writer1.flush();
+
+    }
+}
